7b0b8d00cc930fd6f8c90bde90d2cbbdbadce166
Jefferson Silva <jefferson.bruno.c@conceptatech.com>
2024-11-25 20:09:50 -0300

    chore: install cypress

---
+++ packages/frontend/cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // receives "on" and "config" parameters if needed
+      // implement node event listeners here
+    },
+  },
+});
