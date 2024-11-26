430cc998c6d7ab7df4bf3ff39a343f69ec9b47f1
Jefferson Silva <jefferson.bruno.c@conceptatech.com>
2024-11-25 22:39:08 -0300

    chore: setup cypress testing environment

--- packages/frontend/cypress.config.js
+++ packages/frontend/cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:5173",
     setupNodeEvents() {
       // receives "on" and "config" parameters if needed
       // implement node event listeners here
