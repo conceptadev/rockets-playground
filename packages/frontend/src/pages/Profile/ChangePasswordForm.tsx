f4346d4adea6f4795b6480b8529816c294196aee
Jefferson Silva <jefferson.bruno.c@conceptatech.com>
2025-01-10 17:31:56 -0300

    feat: create profile screen

---
+++ packages/frontend/src/pages/Profile/ChangePasswordForm.tsx
@@ -0,0 +1,66 @@
+import { FC, useState } from "react";
+import { SchemaForm } from "@concepta/react-material-ui";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+import { type PasswordChangeFormData } from "./types";
+import {
+  passwordChangeFormSchema,
+  widgets,
+  advancedProperties,
+  validationRules,
+  validateForm,
+} from "./constants";
+
+interface ChangePasswordFormProps {
+  closePasswordChangeModal: () => void;
+  openConfirmationModal: () => void;
+}
+
+const ChangePasswordForm: FC<ChangePasswordFormProps> = ({
+  closePasswordChangeModal,
+  openConfirmationModal,
+}) => {
+  const [formData, setFormData] = useState<PasswordChangeFormData>({
+    oldPassword: "",
+    newPassword: "",
+    confirmNewPassword: "",
+  });
+
+  const handleFormSubmit = async () => {
+    closePasswordChangeModal();
+    openConfirmationModal();
+  };
+
+  return (
+    <SchemaForm.Form
+      schema={passwordChangeFormSchema}
+      advancedProperties={advancedProperties}
+      onSubmit={handleFormSubmit}
+      widgets={widgets}
+      noHtml5Validate={true}
+      showErrorList={false}
+      formData={formData}
+      onChange={({ formData }) => {
+        setFormData(formData);
+      }}
+      customValidate={(formData, errors) =>
+        validateForm(formData, errors, validationRules)
+      }
+    >
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        mt={4}
+      >
+        <Button type="submit" variant="contained" sx={{ flex: 1, mr: 1 }}>
+          Save
+        </Button>
+      </Box>
+    </SchemaForm.Form>
+  );
+};
+
+export default ChangePasswordForm;
